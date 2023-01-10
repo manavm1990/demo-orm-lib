ae7bbcc771b3db102b3a2f77327dc60273b99b71
Manav Misra <manavm@visionwebsoft.com>
2023-01-09 19:58:59 -0600

    Add model for Book

---
+++ app/book/model.js
@@ -0,0 +1,37 @@
+import { DataTypes } from "sequelize";
+import sequelize from "./conn.js";
+
+const Book = sequelize.define(
+  "Book",
+  {
+    title: {
+      type: DataTypes.STRING,
+    },
+    author: {
+      type: DataTypes.STRING,
+    },
+    isbn: {
+      type: DataTypes.STRING,
+    },
+    pages: {
+      type: DataTypes.INTEGER,
+    },
+    edition: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    // Other model options go here
+    sequelize, // We need to pass the connection instance
+
+    // Set to false to remove `created_at` and `updated_at` fields
+    timestamps: false,
+
+    // Capitalize the model name as shown in the docs.
+    modelName: "Book",
+
+    underscored: true,
+  }
+);
+
+export default Book;
