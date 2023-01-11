ab7903c6c07d34e34013eff4cad69417afba2c79
Manav Misra <manavm@visionwebsoft.com>
2023-01-10 18:43:00 -0600

    feat: :goal_net: 404 catch all

--- app/index.js
+++ app/index.js
@@ -10,6 +10,10 @@ app.use(express.json());
 
 app.use("/api/books", bookRouter);
 
+app.use((_, res) => {
+  res.status(404).json({ message: "Not found" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
